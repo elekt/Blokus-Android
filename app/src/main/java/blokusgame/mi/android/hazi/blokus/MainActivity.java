829677543a3dbd4298de8694c0db814a6a77e8d5
Elekes Tamás <elekestamas22@gmail.com>
2014-11-14 22:37:27 +0100

    fullscreen activity

--- app/src/main/java/blokusgame/mi/android/hazi/blokus/MainActivity.java
+++ app/src/main/java/blokusgame/mi/android/hazi/blokus/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -40,6 +42,10 @@ public class MainActivity extends Activity implements BoardTouchListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
         boardView = (BoardView) findViewById(R.id.boardView);
