a69156d3bfccc542ec067dc2923c2843dc45b3e6
Elekes Tamás <elekestamas22@gmail.com>
2014-11-30 17:41:51 +0100

    Menu show it in the beginning

--- app/src/main/java/blokusgame/mi/android/hazi/blokus/MainActivity.java
+++ app/src/main/java/blokusgame/mi/android/hazi/blokus/MainActivity.java
@@ -67,6 +67,9 @@ public class MainActivity extends Activity implements BoardTouchListener {
         slidingUpLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
         slidingUpLayout.setCoveredFadeColor(Color.TRANSPARENT);
 
+        // show the menu
+        showMenuDialog();
+
         setPlayer(player1);
 
         player2.setEnemy(player1);
@@ -233,39 +236,28 @@ public class MainActivity extends Activity implements BoardTouchListener {
     private void showMenuDialog(){
         // custom dialog
         final Dialog dialog = new Dialog(MainActivity.this);
-        dialog.setContentView(R.layout.game_end);
+        dialog.setContentView(R.layout.game_start);
         dialog.setTitle("Menu");
 
         // set the custom dialog components - text, image and button
-        TextView resultText = (TextView) dialog.findViewById(R.id.btn);
-        TextView firstResult = (TextView) dialog.findViewById(R.id.txt_first_result);
+        Button playerStart = (Button) dialog.findViewById(R.id.btn_player_start);
+        Button AIStart = (Button) dialog.findViewById(R.id.btn_ai_start);
 
-        if(results.x==results.y) {
-            resultText.setText("Its a draw");
-            firstResult.setText("Your points: "+String.valueOf(results.x));
-            secondResult.setText("Opponent's points: "+String.valueOf(results.y));
-        } else {
-            if(results.x>results.y) {
-                resultText.setText("You won!!!");
-                firstResult.setText("Your points: "+String.valueOf(results.x));
-                secondResult.setText("Opponent's points: "+String.valueOf(results.y));
-            } else {
-                resultText.setText("You lost...");
-                firstResult.setText("Opponent's points: "+String.valueOf(results.x));
-                secondResult.setText("Yout points: "+String.valueOf(results.y));
-            }
-        }
 
-        Button dialogButton = (Button) dialog.findViewById(R.id.btn_reset);
-        // if button is clicked, close the custom dialog
-        dialogButton.setOnClickListener(new OnClickListener() {
+        playerStart.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                isHumanFirst = true;
                 dialog.dismiss();
-                reset();
             }
         });
-
+        AIStart.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isHumanFirst = false;
+                dialog.dismiss();
+            }
+        });
         dialog.show();
     }
     private void showResultDialog(Point results){
